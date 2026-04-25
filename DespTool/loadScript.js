869bceaae6b8fedbde7c6baf8aabba8602cde3cc
Felipe Sade <71511092+FeSade1@users.noreply.github.com>
2026-04-25 17:04:32 -0300

    Add loadScript.js to dynamically load popup

---
+++ DespTool/loadScript.js
@@ -0,0 +1,33 @@
+(async () => {
+    const e = document.getElementById("sade-popup-container");
+    if (e) {
+        e.remove();
+        return;
+    }
+    try {
+        const t = await fetch("https://fesade1.github.io/DespTool/popup.html");
+        const n = await t.text();
+        const container = document.createElement("div");
+        container.id = "sade-popup-container";
+        document.body.appendChild(container);
+        const shadow = container.attachShadow({
+            mode: "open"
+        });
+        window.__SADE_SHADOW__ = shadow;
+        shadow.innerHTML = n;
+        shadow.querySelectorAll("script").forEach(oldScript => {
+            const newScript = document.createElement("script");
+            if (oldScript.src) {
+                newScript.src = oldScript.src;
+                newScript.async = false;
+            } else {
+                newScript.textContent = oldScript.textContent;
+            }
+            shadow.appendChild(newScript);
+            oldScript.remove();
+        });
+    } catch (e) {
+        alert("Erro ao carregar popup");
+        alert(e.message);
+    }
+})();
